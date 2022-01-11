a8b4798f2ec5a770726469412f60fd2284f466e1
stb.infoserv <stb.infoserv@gmail.com>
2022-01-11 15:43:04 +0200

    finished all tasks, waiting for review

---
+++ ToggleButtons/ViewModels/DemonstrateViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ToggleButtons.ViewModels
+{
+    internal class DemonstrateViewModel : VMBase
+    {
+        private bool _isEnabled = false;
+        public bool IsEnabled { get => _isEnabled; set { _isEnabled = value; OnPropertyChanged(nameof(IsEnabled)); } }
+        private bool _isOn = false;
+        public bool IsOn { get => _isOn; set { _isOn = value; OnPropertyChanged(nameof(IsOn)); } }
+
+        public ICommand EnableStatusSwitchCommand => new RelayCommand(IsEnableSwitch);
+
+        private void IsEnableSwitch(object o)
+        {
+            if (IsEnabled) 
+            {
+                IsEnabled = false;
+                IsOn = false;
+            }
+            else
+            {
+                IsEnabled = true;
+                IsOn = false;
+            }
+        }
+
+    }
+}
