64af2c45a205df483f8f43d9e52b774baf220987
stb.infoserv <stb.infoserv@gmail.com>
2022-01-14 13:52:51 +0200

    fix

--- MyButton/MyButton.cs
+++ MyButton/MyButton.cs
@@ -1,78 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MyButton
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:MyButton"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:MyButton;assembly=MyButton"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:CustomControl1/>
-    ///
-    /// </summary>
     public class MyButton : Button
     {
-
         public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register("IsOn", typeof(bool), typeof(MyButton), new PropertyMetadata(default));
+        protected async override void OnClick()
+        {
+            base.OnClick();            
+            //timer.Stop();
+            if (IsOn) SetCurrentValue(IsOnProperty, false);
+            else
+            {
+                SetCurrentValue(IsOnProperty, true);
+                await Task.Delay(3000);
+                SetCurrentValue(IsOnProperty, false);
+            }            
+        }
         public bool IsOn
         {
             get { return (bool)GetValue(IsOnProperty); }
             set { SetValue(IsOnProperty, value); }
         }
-        public async void Push(object o)
-        {
-            if (IsOn) IsOn = false;
-            else
-            {
-                IsOn = true;
-                await Task.Run(() => SwitchState());
-                IsOn = _switcher;
-            }
-        }        
+        //DispatcherTimer timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(3) };       
         static MyButton()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyButton), new FrameworkPropertyMetadata(typeof(MyButton)));            
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyButton), new FrameworkPropertyMetadata(typeof(MyButton)));
         }
-        private static bool _switcher;
-        public ICommand PushCommand => new RelayCommand(Push);        
-        private void SwitchState()
+        public MyButton()
         {
-            System.Threading.Thread.Sleep(3000);
-            _switcher = false;            
+            IsEnabledChanged += async (s, e) => 
+            {
+                //timer.Stop();
+                if(IsOn)
+                {
+                    SetCurrentValue(IsOnProperty, true);
+                    await Task.Delay(3000);
+                    SetCurrentValue(IsOnProperty, false);
+                }               
+            };            
         }
     }
 }
